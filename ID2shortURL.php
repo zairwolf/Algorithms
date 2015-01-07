ee49949fe70d83b67c4dba9fabc2e7337a4b8a66
Hai Zheng <z@5ea.com>
2015-01-06 21:23:25 -0500

    Add shortURL2ID function

--- ID2shortURL.php
+++ ID2shortURL.php
@@ -9,15 +9,27 @@
  *	Author: Hai Zheng @ https://www.linkedin.com/in/zairwolf/
  *
  */
-
+//basic define
 $_charArr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 $_len = strlen($_charArr);
 
-echo shortURL(100000000000000);
+//test
+for($i=0;$i<$_len*3;$i++) echo "<br>$i:".ID2shortURL($i);
+echo "<br>c8:".shortURL2ID('c8');
 
-function shortURL($id){
+//*****************function*********************//
+//convert id to url
+function ID2shortURL($id){
 	global $_charArr, $_len;
-	$url = $id >= $_len ? shortURL(floor($id/$_len)) : '';
-	$url .= $_charArr[$id % $_len];
+	$url = $id >= $_len ? ID2shortURL(floor($id/$_len)) : '';
+	$url .= $_charArr[$id%$_len];
 	return $url;
+}
+
+//convert url to id
+function shortURL2ID($url){
+	global $_charArr, $_len;
+	$id = pow($_len, strlen($url)-1)*strpos($_charArr, substr($url, 0, 1));
+	if(strlen($url) > 1) $id += shortURL2ID(substr($url, 1));
+	return $id;
 }

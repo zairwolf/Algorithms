25c31730c8703e2520ef039e88600c2603f56553
Hai Zheng <z@5ea.com>
2015-01-07 13:09:56 -0500

    rename the function

--- ID2shortURL.php
+++ ID2shortURL.php
@@ -1,10 +1,10 @@
 <?php
 /**
- *	Convert ID to short URL in PHP by zairwolf
+ *	Convert id to short URL in PHP by zairwolf
  *
- * 	Demo: http://www.you4be.com/ID2shortURL.php
+ * 	Demo: http://www.you4be.com/id2ShortURL.php
  *
- *	Source: https://github.com/zairwolf/Algorithms/blob/master/ID2shortURL.php
+ *	Source: https://github.com/zairwolf/Algorithms/blob/master/id2ShortURL.php
  *
  *	Author: Hai Zheng @ https://www.linkedin.com/in/zairwolf/
  *
@@ -14,22 +14,22 @@ $_charArr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 $_len = strlen($_charArr);
 
 //test
-for($i=0;$i<$_len*3;$i++) echo "<br>$i:".ID2shortURL($i);
-echo "<br>c8:".shortURL2ID('c8');
+for($i=0;$i<$_len*3;$i++) echo "<br>$i:".id2ShortURL($i);
+echo "<br>c8:".shortURL2Id('c8');
 
 //*****************function*********************//
 //convert id to url
-function ID2shortURL($id){
+function id2ShortURL($id){
 	global $_charArr, $_len;
-	$url = $id >= $_len ? ID2shortURL(floor($id/$_len)) : '';
+	$url = $id >= $_len ? id2ShortURL(floor($id/$_len)) : '';
 	$url .= $_charArr[$id%$_len];
 	return $url;
 }
 
 //convert url to id
-function shortURL2ID($url){
+function shortURL2Id($url){
 	global $_charArr, $_len;
 	$id = pow($_len, strlen($url)-1)*strpos($_charArr, substr($url, 0, 1));
-	if(strlen($url) > 1) $id += shortURL2ID(substr($url, 1));
+	if(strlen($url) > 1) $id += shortURL2Id(substr($url, 1));
 	return $id;
 }
